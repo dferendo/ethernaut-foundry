7832c478ce9f34656654941237587d7d6b493c47
dferendo <dylanfrendo@gmail.com>
2022-06-19 20:24:46 +0100

    Fixed some dependency issue

---
+++ deploy/_03_CoinFlip_Deploy.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.10;
+
+import 'forge-std/Test.sol';
+
+contract _03_CoinFlip_Deploy is Test {
+
+}
--- src/_03_CoinFlip.sol
+++ src/_03_CoinFlip.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.11;
+pragma solidity ^0.8.10;
 
 interface CoinFlip {
   function flip(bool _guess) external returns (bool);
