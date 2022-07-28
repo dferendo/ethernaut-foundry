a64b9bd464bc1ebd34a7a35488e95c8a26852846
dferendo <dylanfrendo@gmail.com>
2022-07-28 22:14:56 +0100

    cleaning

--- deploy/_03_CoinFlip_Deploy.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.10;
-
-import 'forge-std/Test.sol';
-
-contract _03_CoinFlip_Deploy is Test {
-
-}
--- src/Ethernaut.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.12;
-
-interface Ethernaut {
-  function createLevelInstance(address _level) external payable;
-  function submitLevelInstance(address payable _instance) external;
-}
