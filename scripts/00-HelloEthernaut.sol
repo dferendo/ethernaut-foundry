8c1c627c3595bb513d8349fc64fdd9ce405af98c
dferendo <dylanfrendo@gmail.com>
2022-07-28 22:13:36 +0100

    Added scripts for instance creation + submission

---
+++ scripts/00-HelloEthernaut.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.12;
+
+import {Ethernaut} from './base.sol';
+import 'forge-std/Test.sol';
+
+interface IHelloEthernaut {
+  function authenticate(string memory passkey) external;
+  function password() external returns (string memory);
+}
+
+contract HelloEthernaut is Ethernaut {
+
+  function run() public {
+    vm.startBroadcast();
+
+    // Create Instance
+    IHelloEthernaut helloEthernaut = IHelloEthernaut(createNewInstance(HELLO_ETHERNAUT));
+    // Task
+    helloEthernaut.authenticate(helloEthernaut.password());
+    // Submission
+    submitInstance(HELLO_ETHERNAUT, address(helloEthernaut));
+
+    vm.stopBroadcast();
+  }
+}
---
+++ scripts/base.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.12;
+
+import 'forge-std/Test.sol';
+
+interface IEthernaut {
+    function createLevelInstance(address _level) 
+        external 
+        payable;
+
+    function submitLevelInstance(address payable _instance) 
+        external;
+}
+
+interface ILevel {
+    function validateInstance(address payable _instance, address _player)
+        external
+        view
+        returns (bool);
+}
+
+abstract contract Ethernaut is Test {
+  IEthernaut constant ethernaut = IEthernaut(0xD991431D8b033ddCb84dAD257f4821E9d5b38C33);
+
+  // Levels addresses
+  address constant HELLO_ETHERNAUT = address(0x4E73b858fD5D7A5fc1c3455061dE52a53F35d966);
+
+  function createNewInstance(address level)
+      internal
+      returns (address)
+    {
+      vm.recordLogs();
+      // Create normal instance
+      ethernaut.createLevelInstance(level);
+
+      // Get instance address from logs
+      Vm.Log[] memory entries = vm.getRecordedLogs();
+      return abi.decode(entries[0].data, (address));
+    }
+
+    function submitInstance(address level, address instance) internal {
+        require(
+            ILevel(level).validateInstance(payable(instance), msg.sender),
+            "Invalid-Submission"
+        );
+        ethernaut.submitLevelInstance(payable(instance));
+    }
+}
